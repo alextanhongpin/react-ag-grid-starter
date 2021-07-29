5fd2ce927f80ab83399a9c93241d4ecba80bda2b
alextanhongpin <alextan220990@gmail.com>
2021-07-30 01:23:20 +0800

    chore: initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef } from "react";
+import { AgGridColumn, AgGridReact } from "ag-grid-react";
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-alpine.css";
 
 function App() {
+  const gridRef = useRef(null);
+  const rowData = [
+    { make: { value: "Toyota" }, model: "Celica", price: 350000 },
+    { make: { value: "Ford" }, model: "Mondeo", price: 320000 },
+    { make: { value: "Porsche" }, model: "Boxter", price: 720000 },
+  ];
+
+  const handleClick = () => {
+    const selectedNodes = gridRef.current.api.getSelectedNodes();
+    const selectedData = selectedNodes.map((node) => node.data);
+    const selectedDataStringRepresentation = selectedData
+      .map((node) => `${node.make} ${node.model}`)
+      .join(", ");
+    window.alert(selectedDataStringRepresentation);
+  };
+  const handleCellValueChanged = (e) => console.log(e);
+  const makeValueGetter = (params) => params.data.make.value;
+  const makeValueSetter = (params) => {
+    if (params.newValue.startsWith("HAHA")) {
+      return false;
+    }
+    params.data.make.value = params.newValue;
+    return true;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ag-theme-alpine" style={{ height: 400, width: 540 }}>
+      <button onClick={handleClick}>Get selected rows</button>
+      <AgGridReact
+        ref={gridRef}
+        rowSelection="multiple"
+        rowData={rowData}
+        enableMultiRowDragging={true}
+        defaultColDef={{
+          sortable: true,
+          filter: true,
+        }}
+        rowDragManaged={true}
+        animateRows={true}
+      >
+        <AgGridColumn
+          field="make"
+          checkboxSelection={true}
+          rowDrag={true}
+          editable={true}
+          onCellValueChanged={handleCellValueChanged}
+          valueGetter={makeValueGetter}
+          valueSetter={makeValueSetter}
+        ></AgGridColumn>
+        <AgGridColumn field="model"></AgGridColumn>
+        <AgGridColumn field="price"></AgGridColumn>
+      </AgGridReact>
     </div>
   );
 }
